86525e091891e912dcf428686074cfdbcac2d8af
Bassem Dghaidi <bassem@interop.link>
2020-05-24 18:08:45 +0200

    Add webpack config and update build directories

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 const path = require('path')
 
+const entry = path.join(__dirname, '/index.js')
+const output = {
+  path: path.join(__dirname, 'dist'),
+  filename: 'objectron.bundle.js'
+}
+
 module.exports = {
+  node: {
+    fs: 'empty'
+  },
   mode: 'production',
-  entry: './index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'objectron.bundle.js'
-  }
+  entry: entry,
+  output: output
 }
